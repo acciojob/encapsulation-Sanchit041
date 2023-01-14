0c5f528fde9afee570ece7ae7306225a61a89199
gupta <guptasanchit041@gmail.com>
2023-01-14 22:44:50 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
+import static java.io.FileDescriptor.out;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+        r.setMember(100);
+        System.out.print(r.getMember());
+        //java: member has private access in com.driver.RWOnly
+    }
 }
